e324af11b11ed000d6f3746c5240d28784247fd5
Daniel Barclay <barclaysd@me.com>
2019-06-11 21:16:56 +0100

    Created location manager delegate to request user's location to be included to API query

--- iOS-Weather-App/iOS-Weather-App/Controller/WeatherViewController.swift
+++ iOS-Weather-App/iOS-Weather-App/Controller/WeatherViewController.swift
@@ -7,20 +7,48 @@
 //
 
 import UIKit
+import CoreLocation
 
-
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let DARK_SKY_API = "https://api.darksky.net/forecast/fce3d31ef66ca3d79371afec681e88c4/37.8267,-122.4233"
+    let KEY = "fce3d31ef66ca3d79371afec681e88c4"
+    
+    let locationManager = CLLocationManager()
     
     //Pre-linked IBOutlets
     @IBOutlet weak var weatherIcon: UIImageView!
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var temperatureLabel: UILabel!
-
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // set up location manager
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        
+        
+    }
     
-    
+    //MARK: Get location data
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last!
+        if location.horizontalAccuracy > 0 {
+            // valid result found
+            locationManager.stopUpdatingLocation()
+            
+            let params: [String: String] = ["lat": String(location.coordinate.latitude), "long": String(location.coordinate.longitude)]
+            
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+        cityLabel.text = "Location Unavailable"
     }
 }
 
